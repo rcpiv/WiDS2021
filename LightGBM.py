f24f8b5c4ec5c31895cf29434a5e58d0214b8f38
Robert C Patterson <67168947+rcpiv@users.noreply.github.com>
2021-02-18 14:07:43 -0500

    Add files via upload

---
+++ LightGBM.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 21:56:03 2021
+
+@author: Robby
+"""
+
+#%% Import Packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Load in Data
+import os
+os.chdir(r"C:\Users\Robby\Desktop\IAA\Personal Projects\WiDS")
+
+train = pd.read_csv(r'train_split.csv')
+valid = pd.read_csv(r'valid.csv')
+
+train2 = pd.read_csv(r'train_imput_split.csv')
+valid2 =  pd.read_csv(r'valid_imput.csv')
+
+#%% Split off target column
+train_y = train.diabetes_mellitus
+train_x = train.drop('diabetes_mellitus', axis=1)
+
+train2_y = train2.diabetes_mellitus
+train2_x = train2.drop('diabetes_mellitus', axis=1)
+
+valid_y = valid.diabetes_mellitus
+valid_x = valid.drop('diabetes_mellitus', axis=1)
+
+valid2_y = valid2.diabetes_mellitus
+valid2_x = valid2.drop('diabetes_mellitus', axis=1)
+
+#%% Get dummies
+train_x = pd.get_dummies(train_x)
+train2_x = pd.get_dummies(train2_x)
+valid_x = pd.get_dummies(valid_x)
+valid2_x = pd.get_dummies(valid2_x)
+
+
+#%% Downsample
+from sklearn.model_selection import train_test_split as tts
+train_x_ds, a, train_y_ds, b = tts(train_x,train_y, test_size = .9, random_state= 1234)
+
+#%% Set Up
+import lightgbm as lgb
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_auc_score
+
+ss = StandardScaler()
+train_x_ds = ss.fit_transform(train_x_ds)
+valid_x = ss.transform(valid_x)
+#%% Set Params
+params = {
+    'learning_rate' : .3,
+    'boosting_type' : 'gbdt',
+    'objective' : 'binary',
+    'metric' : 'auc',
+    'sub_feature' : .8,
+    'num_leaves' : 5000,
+    'min_data' : 20,
+    'max_depth' : 10,
+    'feature_pre_filter': False,
+    'verbose': -1
+    }
+
+d_train = lgb.Dataset(train_x_ds, label=train_y_ds)
+#%% LGBM
+lgbm = lgb.cv(params, d_train, num_boost_round= 100, nfold=5, early_stopping_rounds= 10, seed = 42)
+print(np.max(lgbm['auc-mean'])) # 0.818
+
+#%% Parameter Tuning
+gridsearch_params = [
+    (min_data, max_depth)
+    for min_data in range(43,46,1)
+    for max_depth in range (3,4)]
+        
+max_AUC = float(0)
+best_params = None
+for min_data, max_depth in gridsearch_params:
+    print("CV with max_depth={}, min_child_weight={}".format(
+                             max_depth,
+                             min_data))
+    # Update our parameters
+    params['max_depth'] = max_depth
+    params['min_data'] = min_data
+    # Run CV
+    cv_results = lgb.cv(
+        params,
+        d_train,
+        num_boost_round=100,
+        seed=42,
+        nfold=5,
+        metrics={'auc'},
+        #early_stopping_rounds=10
+    )
+    # Update best MAE
+    mean_auc = np.max(cv_results['auc-mean'])
+    print("\tAUC {}".format(mean_auc))
+    if mean_auc > max_AUC:
+        max_AUC = mean_auc
+        best_params = (3,min_data)
+print("Best params: {}, {}, AUC: {}".format(best_params[0], best_params[1], max_AUC))
+
+#%% Set Params
+params['max_depth'] = 3
+params['min_data'] = 43
+
+#%% Parameter Tuning
+max_AUC = float(0)
+best_params = None
+for num_leaves in range(2,22,1):
+    print("CV with num_leaves={}".format(num_leaves))
+    # Update our parameters
+    params['num_leaves'] = num_leaves
+    # Run CV
+    cv_results = lgb.cv(
+        params,
+        d_train,
+        num_boost_round=100,
+        seed=42,
+        nfold=5,
+        metrics={'auc'},
+        #early_stopping_rounds=10
+    )
+    # Update best MAE
+    mean_auc = np.max(cv_results['auc-mean'])
+    print("\tAUC {}".format(mean_auc))
+    if mean_auc > max_AUC:
+        max_AUC = mean_auc
+        best_params = (num_leaves)
+print("Best params: {}, AUC: {}".format(best_params, max_AUC))
+
+#%% Parameter Tuning
+max_AUC = float(0)
+best_params = None
+for colsample in range(31,52,1):
+    print("CV with colsample={}".format(colsample/100))
+    # Update our parameters
+    params['sub_feature'] = colsample/100
+    # Run CV
+    cv_results = lgb.cv(
+        params,
+        d_train,
+        num_boost_round=100,
+        seed=42,
+        nfold=5,
+        metrics={'auc'},
+        #early_stopping_rounds=10
+    )
+    # Update best MAE
+    mean_auc = np.max(cv_results['auc-mean'])
+    print("\tAUC {}".format(mean_auc))
+    if mean_auc > max_AUC:
+        max_AUC = mean_auc
+        best_params = (colsample)
+print("Best params: {}, AUC: {}".format(best_params, max_AUC))
+
+#%% Set Param
+params['num_leaves'] = 8
+params['sub_feature'] = .44
+#%% Parameter Tuning
+max_AUC = float(0)
+best_params = None
+for eta in range(21,32,1):
+    print("CV with eta={}".format(eta/1000))
+    # Update our parameters
+    params['learning_rate'] = eta/100
+    # Run CV
+    cv_results = lgb.cv(
+        params,
+        d_train,
+        num_boost_round=100,
+        seed=42,
+        nfold=5,
+        metrics={'auc'},
+        #early_stopping_rounds=10
+    )
+    # Update best MAE
+    mean_auc = np.max(cv_results['auc-mean'])
+    print("\tAUC {}".format(mean_auc))
+    if mean_auc > max_AUC:
+        max_AUC = mean_auc
+        best_params = (eta/100)
+print("Best params: {}, AUC: {}".format(best_params, max_AUC))
+
+#%% Set Params
+params['learning_rate'] = .025
+
+#%% Check Dart
+params['boosting_type'] = 'dart'
+cv_results = lgb.cv(
+       params,
+       d_train,
+       num_boost_round = 250,
+       seed=42,
+       nfold=5,
+       metrics='auc'
+       )
+
+# Update best MAE
+mean_auc = np.max(cv_results['auc-mean'])
+print("\tAUC {}".format(mean_auc))
+
+#%% Best Model
+model = lgb.train(
+    params,
+    d_train,
+    num_boost_round = 250,
+    )
+
+preds = model.predict(train_x_ds)
+roc_auc_score(train_y_ds, preds)
+
+#%% Fit to whole data
+d_train2 = lgb.Dataset(train_x,label= train_y)
+d_valid = lgb.Dataset(valid_x, label=valid_y)
+
+model2 = lgb.train(params,
+                   d_train2,
+                   num_boost_round= 250)
+
+preds2= model2.predict(train_x)
+print(roc_auc_score(train_y, preds2))
+
+#%% Validate Overfit AF
+predsv = model2.predict(valid_x)
+print(roc_auc_score(valid_y,predsv))
---
+++ XGBoost.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 15:15:25 2021
+
+@author: Robby
+"""
+
+#%% Import Packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Load in Data
+import os
+os.chdir(r"C:\Users\Robby\Desktop\IAA\Personal Projects\WiDS")
+
+train = pd.read_csv(r'train_split.csv')
+valid = pd.read_csv(r'valid.csv')
+
+train2 = pd.read_csv(r'train_imput_split.csv')
+valid2 =  pd.read_csv(r'valid_imput.csv')
+
+#%% Split off target column
+train_y = train.diabetes_mellitus
+train_x = train.drop('diabetes_mellitus', axis=1)
+
+train2_y = train2.diabetes_mellitus
+train2_x = train2.drop('diabetes_mellitus', axis=1)
+
+valid_y = valid.diabetes_mellitus
+valid_x = valid.drop('diabetes_mellitus', axis=1)
+
+valid2_y = valid2.diabetes_mellitus
+valid2_x = valid2.drop('diabetes_mellitus', axis=1)
+
+#%% Get dummies
+train_x = pd.get_dummies(train_x)
+train2_x = pd.get_dummies(train2_x)
+valid_x = pd.get_dummies(valid_x)
+valid2_x = pd.get_dummies(valid2_x)
+
+
+#%% Downsample
+from sklearn.model_selection import train_test_split as tts
+train_x_ds, a, train_y_ds, b = tts(train_x,train_y, test_size = .9, random_state= 1234)
+
+#%% XGBoost
+import xgboost as xgb
+
+dtrain = xgb.DMatrix(train_x,label=train_y)
+dtest = xgb.DMatrix(valid_x,label=valid_y)
+
+params = {
+    # Parameters that we are going to tune.
+    'max_depth':6,
+    'min_child_weight': 1,
+    'eta':.3,
+    'subsample': 1,
+    'colsample_bytree': 1,
+    # Other parameters
+    'objective':'binary:logistic',
+}
+
+params['eval_metric'] ='auc'
+num_boost_rounds = 250
+
+model = xgb.train(
+    params,
+    dtrain,
+    num_boost_round=num_boost_rounds,
+    evals=[(dtest,'Test')],
+    early_stopping_rounds=10
+    )
+
+print("Best AUC: {:.2f} with {} rounds".format(
+                 model.best_score,
+                 model.best_iteration+1))
+
+cv_results = xgb.cv(
+    params,
+    dtrain,
+    num_boost_round=num_boost_rounds,
+    seed=1234,
+    nfold=5,
+    metrics={'auc'},
+    early_stopping_rounds=10
+)
+cv_results
+cv_results['test-auc-mean'].max()
+#%% Grid for max depth and child weight
+gridsearch_params = [
+    (max_depth, min_child_weight)
+    for max_depth in range(3,15,3)
+    for min_child_weight in range(1,10,2)]
+        
+# Define initial best params and AUC
+max_AUC = float(0)
+best_params = None
+for max_depth, min_child_weight in gridsearch_params:
+    print("CV with max_depth={}, min_child_weight={}".format(
+                             max_depth,
+                             min_child_weight))
+    # Update our parameters
+    params['max_depth'] = max_depth
+    params['min_child_weight'] = min_child_weight
+    # Run CV
+    cv_results = xgb.cv(
+        params,
+        dtrain,
+        num_boost_round=num_boost_rounds,
+        seed=42,
+        nfold=5,
+        metrics={'auc'},
+        early_stopping_rounds=10
+    )
+    # Update best MAE
+    mean_auc = cv_results['test-auc-mean'].max()
+    boost_rounds = cv_results['test-auc-mean'].argmax()
+    print("\tAUC {} for {} rounds".format(mean_auc, boost_rounds))
+    if mean_auc > max_AUC:
+        max_AUC = mean_auc
+        best_params = (max_depth,min_child_weight)
+print("Best params: {}, {}, AUC: {}".format(best_params[0], best_params[1], max_AUC))
+
+#%% 3 and 3
+params['max_depth'] = 3
+params['min_child_weight'] = 3
+
+#%% subsample and colsample
+gridsearch_params = [
+    (subsample, colsample)
+    for subsample in [i/10. for i in range(6,11)]
+    for colsample in [i/10. for i in range(6,11)]
+]
+
+max_auc = float(0)
+best_params = None
+# We start by the largest values and go down to the smallest
+for subsample, colsample in reversed(gridsearch_params):
+    print("CV with subsample={}, colsample={}".format(
+                             subsample,
+                             colsample))
+    # We update our parameters
+    params['subsample'] = subsample
+    params['colsample_bytree'] = colsample
+    # Run CV
+    cv_results = xgb.cv(
+        params,
+        dtrain,
+        num_boost_round=num_boost_rounds,
+        seed=42,
+        nfold=5,
+        metrics={'auc'},
+        early_stopping_rounds=10
+    )
+    # Update best score
+    mean_auc = cv_results['test-auc-mean'].max()
+    boost_rounds = cv_results['test-auc-mean'].argmax()
+    print("\tAUC {} for {} rounds".format(mean_auc, boost_rounds))
+    if mean_auc > max_auc:
+        max_auc = mean_auc
+        best_params = (subsample,colsample)
+print("Best params: {}, {}, AUC: {}".format(best_params[0], best_params[1], max_auc))
+
+#%% Assign 1&1
+params['subsample'] = 1
+params['colsample'] = 1
+
+#%% ETA
+# This can take some time…
+max_auc = float(0)
+best_params = None
+for eta in [.3, .2, .1, .05, .01, .005]:
+    print("CV with eta={}".format(eta))
+    # We update our parameters
+    params['eta'] = eta
+    # Run and time CV
+    cv_results = xgb.cv(
+            params,
+            dtrain,
+            num_boost_round=num_boost_rounds,
+            seed=1234,
+            nfold=5,
+            metrics=['auc'],
+            early_stopping_rounds=10
+          )
+    # Update best score
+    mean_auc = cv_results['test-auc-mean'].max()
+    boost_rounds = cv_results['test-auc-mean'].argmax()
+    print("\tAUC {} for {} rounds\n".format(mean_auc, boost_rounds))
+    if mean_auc > max_auc:
+        max_auc = mean_auc
+        best_params = eta
+print("Best params: {}, AUC: {}".format(best_params, max_auc))
+
+#%% ETA
+params['eta'] = .2
+
+#%% Test
+model = xgb.train(
+    params,
+    dtrain,
+    num_boost_round=num_boost_rounds,
+    evals=[(dtest, "Test")],
+    early_stopping_rounds=10)
+
+print("Best AUC: {:.2f} in {} rounds".format(model.best_score, model.best_iteration+1))
+
+#%% Best Model
+num_boost_round = model.best_iteration + 1
+best_model = xgb.train(
+    params,
+    dtrain,
+    num_boost_round=num_boost_round,
+    evals=[(dtest, "Test")]
+)
+
+#%% Preds
+preds = best_model.predict(dtest)
+from sklearn.metrics import roc_auc_score as auc
+
+auc(valid_y, preds)
+#%%
+# model = xgb.XGBClassifier(silent=False,
+#                     scale_pos_weight=1,
+#                     learning_rate=.1,
+#                     colsample_bytree=0.4,
+#                     subsample=.9,
+#                     objective='binary:logistic',
+#                     n_estimators = 1000,
+#                     reg_alpha = 0.3,
+#                     max_depth = 7,
+#                     gamma = 5
+#                     )
+  
+model.fit(train_x, train_y)
+preds = model.predict(valid_x)
+  
+from sklearn.metrics import roc_auc_score as auc
+auc_score = auc(valid_y, preds)
+print(auc_score)
